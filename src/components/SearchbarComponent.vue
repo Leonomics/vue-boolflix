255ea151eeeebcf3c174fc17026126e7ab862c21
Leonomics <leocv97@gmail.com>
2022-09-09 17:14:05 +0200

    add SearchbarComponent

---
+++ src/components/SearchbarComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="form-group">
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'SearchbarComponent',
+  data(){
+    return{
+      api_key: '6774fac658b7c631bbcfd6f459c7eee5',
+    }
+  },
+  props: {
+  },
+
+  created() {
+        axios
+            .get("https://developers.themoviedb.org/3/search/movie")
+            .then((res) => {
+            console.log(res.data);
+            this.listDisks = res.data.response;
+        });
+    },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
